9f92d0058394374ac981a078691f015300d1841f
Tobias Termeczky <tobiastermeczky@hotmail.com>
2017-09-20 14:49:23 +0200

    Werkt niet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './component/app.component';
+import { AppComponent } from './app.component';
 import {HttpModule} from '@angular/http';
 import {BlindWallService} from './services/BlindWall.service';
 import {RouteService} from './services/Route.service';
