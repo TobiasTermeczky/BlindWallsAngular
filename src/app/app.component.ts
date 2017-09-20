d1f4f2686cefdd6660efac346aee88bca8e86b21
Pascal van Hoof <itsmepascal@gmail.com>
2017-09-20 12:14:41 +0200

    BlindWallService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {BlindWallService} from "./services/BlindWall.service";
+import {BlindWall} from "./model/blindWall/BlindWall";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  public BlindWalls:BlindWall[];
+
+
+  constructor(private BlindWallService:BlindWallService) {
+    //...eventuele extra initialisaties
+  }
+
+  ngOnInit() {
+    this.getBlindWalls();
+  }
+
+
+  //***********************
+  // implementation
+  //***********************
+  getBlindWalls() {
+    if (!this.BlindWalls) {
+      this.BlindWallService.getBlindWalls()
+        .subscribe(BlindWallData => {
+            this.BlindWalls = BlindWallData.json();				// 1. success handler
+            console.log(this.BlindWalls);
+          },
+          err => console.log(err),						// 2. error handler
+          ()=> console.log('Getting cities complete...')	// 3. complete handler
+        )
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HttpModule} from "@angular/http";
+import {BlindWallService} from "./services/BlindWall.service";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpModule
   ],
-  providers: [],
+  providers: [BlindWallService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
